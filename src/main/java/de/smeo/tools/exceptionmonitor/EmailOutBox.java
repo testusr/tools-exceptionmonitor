ba8b80b4bc1ac24df327aba4ea7e4cfa5878be98
Thorsten Rühl <smeo@gmx.de>
2011-10-26 06:21:49 +0200

    added untested email sending mechanism

---
+++ src/main/java/de/smeo/tools/exceptionmonitor/EmailOutBox.java
@@ -0,0 +1,102 @@
+package de.smeo.tools.exceptionmonitor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+
+import de.smeo.tools.exceptionmonitor.SingleFileExceptionReport.ReportedExceptionOccurances;
+
+public class EmailOutBox {
+	List<ExceptionReportEmail> preparedEmails = new ArrayList<EmailOutBox.ExceptionReportEmail>();
+
+	public void emailExceptionToRecipient(
+			EmailAdress emailAdress, MonitoredFile monitoredFile, ReportedExceptionOccurances exception) {
+		ExceptionReportEmail exceptionReportEmail = getOrCreatePreparedExceptionReportEmail(emailAdress, monitoredFile.getFilename());
+		exceptionReportEmail.addReportedExcpetion(exception);
+	}
+	
+	private ExceptionReportEmail getOrCreatePreparedExceptionReportEmail(
+			EmailAdress emailAdress, String filename) {
+		ExceptionReportEmail preparedEmail = getExceptionReportEmail(emailAdress, filename);
+		if (preparedEmail == null){
+			preparedEmail = new ExceptionReportEmail(emailAdress, filename);
+		}
+		return preparedEmail;
+	}
+	
+	private ExceptionReportEmail getExceptionReportEmail(EmailAdress emailAdress, String filename){
+		for (ExceptionReportEmail currExceptionReportEmail : preparedEmails){
+			if (currExceptionReportEmail.getEmailAdress().equals(emailAdress)
+				&& currExceptionReportEmail.getFilename().equals(filename)){
+					return currExceptionReportEmail;
+				}
+		}
+		return null;
+	}
+
+		
+
+	public void sendEmails() {
+	      Properties props = new Properties();
+	      props.setProperty("mail.transport.protocol", "smtp");
+	      props.setProperty("mail.host", "mymail.server.org");
+	      props.setProperty("mail.password", "");
+	      props.setProperty("mail.user", "truehl");
+
+	      for (ExceptionReportEmail currEmail : preparedEmails){
+		      try {
+			      Session mailSession = Session.getDefaultInstance(props, null);
+			      Transport transport = mailSession.getTransport();
+		
+			      MimeMessage message = new MimeMessage(mailSession);
+			      message.setSubject(currEmail.getSubject());
+			      message.setContent(currEmail.getText(), "text/plain");
+			      message.addRecipient(Message.RecipientType.TO,
+			           new InternetAddress(currEmail.getEmailAdress().getAdressString()));
+		
+			      transport.connect();
+			      transport.sendMessage(message,
+			          message.getRecipients(Message.RecipientType.TO));
+			      transport.close();
+		      } catch (MessagingException e){
+		    	  e.printStackTrace();
+		      }
+		 }
+		
+	}
+
+	private static class ExceptionReportEmail {
+		private final EmailAdress emailAdress;
+		private final String filename;
+		private ExceptionReport exceptionReport = new ExceptionReport();
+		
+		public ExceptionReportEmail(EmailAdress emailAdress, String filename) {
+			this.emailAdress = emailAdress;
+			this.filename = filename;
+		}
+
+		public String getFilename() {
+			return filename;
+		}
+
+		public EmailAdress getEmailAdress() {
+			return emailAdress;
+		}
+
+		public String getSubject(){
+			return "ExceptionReport: " + filename;
+		}
+		
+		public String getText(){
+			return exceptionReport.toString();
+		}
+
+		public void addReportedExcpetion(ReportedExceptionOccurances exception) {
+			exceptionReport.addReportedExceptionOccurances(exception);
+		}
+		
+	}
+}
---
+++ src/main/java/de/smeo/tools/exceptionmonitor/ExceptionReport.java
@@ -0,0 +1,97 @@
+package de.smeo.tools.exceptionmonitor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import de.smeo.tools.exceptionmonitor.SingleFileExceptionReport.ReportedExceptionOccurances;
+import de.smeo.tools.exceptionmonitor.exceptionparser.ExceptionCausedByChain;
+
+public class ExceptionReport {
+	private List<NamedException> namedExceptionChains = new ArrayList<NamedException>();
+	
+	
+	public void addReportedExceptionOccurances(ReportedExceptionOccurances reportedExceptionOccurances){
+		this.namedExceptionChains.add(new NamedException(reportedExceptionOccurances));
+	}
+	
+	@Override
+	public String toString() {
+		sortNamedChainsAndSetNames();
+		StringBuffer stringBuffer = new StringBuffer();
+		stringBuffer.append(getShortDescriptions());
+		stringBuffer.append("\n\n\n");
+		stringBuffer.append(getFullDescriptions());
+		return stringBuffer.toString();
+	}
+	
+	
+	private String getFullDescriptions() {
+		StringBuffer stringBuffer = new StringBuffer();
+		stringBuffer.append("----------------------------------------");
+		stringBuffer.append("Full Description");
+		stringBuffer.append("----------------------------------------");
+		stringBuffer.append("Note: the exception stacktraces present the");
+		stringBuffer.append("first occuring exception, the comments for ");
+		stringBuffer.append("the other listed occurances can be different.");
+		stringBuffer.append("----------------------------------------\n");
+		for (NamedException currExceptionChain : namedExceptionChains){
+			stringBuffer.append(currExceptionChain.getFullDescription());
+		}
+		return stringBuffer.toString();
+	}
+
+	private String getShortDescriptions() {
+		StringBuffer stringBuffer = new StringBuffer();
+		stringBuffer.append("----------------------------------------");
+		stringBuffer.append("Short Description");
+		stringBuffer.append("----------------------------------------");
+		for (NamedException currExceptionChain : namedExceptionChains){
+			stringBuffer.append(currExceptionChain.getShortDescription());
+		}
+		return stringBuffer.toString();
+	}
+
+	private void sortNamedChainsAndSetNames(){
+		int i=1;
+		Collections.sort(namedExceptionChains);
+		for (NamedException currExceptionChain : namedExceptionChains){
+			currExceptionChain.setName("No." + i++);
+		}
+	}
+	
+	private static class NamedException implements Comparable<NamedException>{
+		private String name;
+		private ReportedExceptionOccurances reportedExceptionOccurances;
+		
+		public NamedException(
+				ReportedExceptionOccurances reportedExceptionOccurances) {
+			this.reportedExceptionOccurances = reportedExceptionOccurances;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		private ExceptionCausedByChain getSampleExceptionChain() {
+			return reportedExceptionOccurances.getReportedException().getSampleExceptionChain();
+		}
+		
+		public String getShortDescription(){
+			return getSampleExceptionChain().getFirstExceptionName();
+		}
+		
+		public String getFullDescription(){
+			return "Exception[" + name + "/ count: "+reportedExceptionOccurances.occuranceCount() +"]\n" +
+					getSampleExceptionChain().toString();
+		}
+		
+		public int compareTo(NamedException o) {
+			int result = (new Integer(o.reportedExceptionOccurances.occuranceCount()).compareTo(o.reportedExceptionOccurances.occuranceCount()));
+			if (result == 0){
+				(o.getSampleExceptionChain().getFirstExceptionName()).compareTo(getSampleExceptionChain().getFirstExceptionName());
+			}
+			return result;
+		}
+	}
+}
