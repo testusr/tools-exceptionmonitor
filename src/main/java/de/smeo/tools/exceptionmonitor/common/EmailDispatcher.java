bd201713dae77caa7b2ce96e41ef4105197b1700
testusr <smeo@gmx.de>
2012-01-12 22:12:28 +0100

    added email list support

--- src/main/java/de/smeo/tools/exceptionmonitor/common/EmailDispatcher.java
+++ src/main/java/de/smeo/tools/exceptionmonitor/common/EmailDispatcher.java
@@ -13,13 +13,12 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-
 public class EmailDispatcher {
 	private Properties mailServerProperties;
 
 	public EmailDispatcher(String configfile) {
 		File file = new File(configfile);
-		if (file.exists()){
+		if (file.exists()) {
 			try {
 				mailServerProperties = FileUtils.loadPropertiesFromFile(file);
 			} catch (IOException e) {
@@ -29,35 +28,38 @@ public class EmailDispatcher {
 			createSamplePropertyFile(file);
 		}
 	}
-	
-	public void sendEmail(String receiver, String subject, String messageBody) throws MessagingException{
-		System.out.println("---------------------------------------------------------\n");
-		System.out.println("Sending email: '" + subject + "'\n");
-		System.out.println("receiver: '" + receiver + "'\n");
-		System.out.println("messageBody: \n" + messageBody + "\n");
-		System.out.println("---------------------------------------------------------\n");
 
-		Session mailSession = Session.getDefaultInstance(mailServerProperties, null);
-		Transport transport = mailSession.getTransport();
-		
-		MimeMessage message = new MimeMessage(mailSession);
-		message.setSubject(subject);
-		message.setContent(messageBody, "text/plain");
-		message.addRecipient(Message.RecipientType.TO,
-			           new InternetAddress(receiver));
-		
-		transport.connect();
-		transport.sendMessage(message,
-		   message.getRecipients(Message.RecipientType.TO));
-		transport.close();
+	public void sendEmail(String receiverList, String subject,
+			String messageBody) throws MessagingException {
+		String receivers[] = receiverList.split(",");
+
+		for (String receiver : receivers) {
+			System.out.println("---------------------------------------------------------\n");
+			System.out.println("Sending email: '" + subject + "'\n");
+			System.out.println("receiver: '" + receiver + "'\n");
+			System.out.println("messageBody: \n" + messageBody + "\n");
+			System.out.println("---------------------------------------------------------\n");
+
+			Session mailSession = Session.getDefaultInstance(
+					mailServerProperties, null);
+			Transport transport = mailSession.getTransport();
+
+			MimeMessage message = new MimeMessage(mailSession);
+			message.setSubject(subject);
+			message.setContent(messageBody, "text/plain");
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(
+					receiver));
+
+			transport.connect();
+			transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
+			transport.close();
+		}
 	}
 
-
-    
-	static void createSamplePropertyFile(File file){
+	static void createSamplePropertyFile(File file) {
 		try {
 			boolean fileCreated = file.createNewFile();
-			if (fileCreated){
+			if (fileCreated) {
 				FileWriter fstream = new FileWriter(file);
 				BufferedWriter ofile = new BufferedWriter(fstream);
 				ofile.write("mail.transport.protocol=smpt\n");
@@ -66,9 +68,10 @@ public class EmailDispatcher {
 				ofile.write("mail.user=<your user>\n");
 				ofile.close();
 			} else {
-				System.err.println("Could not create file: '" + file.getAbsolutePath() + "'");
+				System.err.println("Could not create file: '"
+						+ file.getAbsolutePath() + "'");
 			}
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
